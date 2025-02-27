593517c5c2e86242d89c01e642673c0cf5799c9b
Edrich Barnard <edrichbarnard@gmail.com>
2025-02-27 21:51:57 +0200

    feature: implement build settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,45 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+export default defineConfig(({ mode }) => {
+  if (mode === 'development') {
+    return {
+      mode: mode,
+      appType: 'spa',
+      plugins: [svelte()],
+      build: {
+        outDir: './dist/dev',
+        sourcemap: true,
+      }
+    }
+  } else if (mode === 'staging') {
+    return {
+      mode: mode,
+      appType: 'spa',
+      plugins: [svelte()],
+      build: {
+        outDir: './dist/stage',
+      }
+    }
+  } else if (mode === 'production') {
+    return {
+      mode: mode,
+      appType: 'spa',
+      plugins: [svelte()],
+      build: {
+        outDir: './dist/prod',
+      }
+    }
+  }
+
+  // default
+  return {
+    mode: mode,
+    appType: 'spa',
+    plugins: [svelte()],
+    build: {
+      outDir: './dist/dev',
+      sourcemap: true
+    }
+  }
 })
